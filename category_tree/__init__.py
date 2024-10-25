813a49d85d6db1e145f052e9bae11fd7c1d56dcb
cdalinghaus <108818676+cdalinghaus@users.noreply.github.com>
2024-10-25 14:05:03 +0200

    Update __init__.py

--- category_tree/__init__.py
+++ category_tree/__init__.py
@@ -1,4 +1,4 @@
-from ._category_tree import categories, build_category_tree, display_category_hierarchy
+from ._category_tree import categories, build_category_tree, display_category_hierarchy, get_implied_categories
 
 def test():
     print("Tree test :)")
